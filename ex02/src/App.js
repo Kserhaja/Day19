30d6180407bbb08eaf57f12de4c3152cd712fd39
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-26 19:39:06 +0100

    Hello Padawans

--- ex02/src/App.js
+++ ex02/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>"Hello Padawans"</code> and save to reload.
+          <code>Hello Padawans</code>
         </p>
         <a
           className="App-link"
