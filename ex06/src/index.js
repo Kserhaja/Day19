564bcf639fa5f51c42336f861a93fddf3978fdd8
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-26 22:03:53 +0100

    hello world

---
+++ ex06/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+React.Component()
+class App extends React.Component {
+  render() {
+
+    const world = "World"
+    return (
+      <div className="App">
+        <h1>Hello, {world}!</h1>
+      </div>
+    )
+  }
+}
+// add the following code bellow this line
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
